2d3d9e8b1d2a957388bce323469334bb987cafeb
Florekem-125 <m.florczyk@nencki.edu.pl>
2024-08-09 15:00:01 +0000

    sparse matrix setup

--- scREP_projectTIL_tut/scripts/20k_BMMNCs.R
+++ scREP_projectTIL_tut/scripts/20k_BMMNCs.R
@@ -6,3 +6,35 @@ library(patchwork)
 library(ProjecTILs)
 library(scRepertoire)
 library(tidyverse)
+
+getwd()
+
+# --- seurat sparse matrix ---------------------------------------------
+sparse_matrix <- Read10X(
+  "singleCell_LAB/scREP_projectTIL_tut/data/20k_BMMNCs/gex/"
+)
+names(sparse_matrix)
+# [1] "Gene Expression"  "Antibody Capture"
+gex <- sparse_matrix$`Gene Expression`
+dim(gex)
+# [1] 36601 17645
+adt <- sparse_matrix$`Antibody Capture`
+dim(adt)
+# [1]   137 17645 [TotalSeq C Human Universal antibody cocktail]
+
+# --- create seuratObject -----------------------------------------------------
+seu <- CreateSeuratObject(
+  gex,
+  project = "20k_BMMNCs"
+)
+seu
+# An object of class Seurat
+# 36601 features across 17645 samples within 1 assay
+# Active assay: RNA (36601 features, 0 variable features)
+#  1 layer present: counts
+
+# --- filter seuratObject ----------------------------------------------------
+
+# --- scRep prepare T data -----------------------------------------------------
+
+# --- scRep prepare B data -----------------------------------------------------
