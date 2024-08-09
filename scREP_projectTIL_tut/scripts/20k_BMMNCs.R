f3272c151d1c74b31379a01a1d5859817188ac73
Florekem-125 <m.florczyk@nencki.edu.pl>
2024-08-09 14:44:39 +0000

    20k_BMMNCs init

---
+++ scREP_projectTIL_tut/scripts/20k_BMMNCs.R
@@ -0,0 +1,8 @@
+# https://www.10xgenomics.com/datasets/20-k-bone-marrow-mononuclear-cells-bmmn-cs-5-ht-v-2-0-2-high-6-1-0 #nolint
+
+library(Seurat)
+library(ggplot2)
+library(patchwork)
+library(ProjecTILs)
+library(scRepertoire)
+library(tidyverse)
